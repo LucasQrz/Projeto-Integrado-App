d63c651f8678be88d2b022f9901e97d9b172a8d6
LucasQrz <jllq1@aluno.ifal.edu.br>
2022-12-18 11:10:48 -0300

    Adição dos filmes na tela de ListaFilmes

--- src/Sistema/ListaFilmes.js
+++ src/Sistema/ListaFilmes.js
@@ -1,12 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { View, FlatList, TouchableOpacity, Image } from 'react-native';
 
-const ListaFilmes = () => {
+export default function ListaFilmes({ navigation }) {
+  const [filmes, setFilmes] = useState([]);
+
+  const getFilmes = async () => {
+    try {
+      const resposta = await fetch(
+        'https://api.themoviedb.org/3/movie/popular?api_key=3e8dec90feebc5e7d11344d90f9d75fe&language=pt-BR&page=1'
+      );
+      const json = await resposta.json();
+      setFilmes(json.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getFilmes();
+  }, []);
+
   return (
-    <View style={{flex: 1, backgroundColor: '#000000'}}>
-      <Text></Text>
+    <View style={{backgroundColor: '#000000'}}>
+      <FlatList
+        data={filmes}
+        renderItem={({ item }) => (
+          
+          <TouchableOpacity
+          onPress={() => navigation.navigate('Detalhes', { id: item.id })}>
+            
+            <Image style={{height: 250, margin: 10}}
+              resizeMode='contain'
+              source={{ uri: 'https://image.tmdb.org/t/p/w200/' + item.poster_path}}
+            />
+          </TouchableOpacity>
+        )}
+      />
     </View>
-  )
+  );
 }
-
-export default ListaFilmes
