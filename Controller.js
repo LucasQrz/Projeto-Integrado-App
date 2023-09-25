eb8e4ef98ef93606ef5c1079ecd77bcd8e29970e
jhonattanRamon <jhonattan.ramon.17@gmail.com>
2023-09-25 18:44:22 -0300

    .

--- Controller.js
+++ Controller.js
@@ -1,23 +1,20 @@
 const express=require('express');
 const bodyParser=require('body-parser');
-
 const cors=require('cors');
-
 
 let app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json);
+app.use(express.json());
 
 //Routes
 app.post('/create', async(req,res)=>{
-        console.log(req.body.emailUser);
-        
+        console.log("chegou");
+        console.log(req.body.emailUser)
 })
-
 
 //Start server
 let port = process.env.PORT || 3000;
 app.listen(port,(req,res)=>{
-    console.log('Servidor Rodando');
+    console.log(port,'Servidor Rodando');
 })
