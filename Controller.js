7d03dd11d3a6edf091d27c3e2d54fb13fd90ee44
JonataMoura <jrms7@aluno.ifal.edu.br>
2023-10-03 11:20:01 -0300

    alteração para o banco de dados

--- Controller.js
+++ Controller.js
@@ -1,25 +1,29 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 const cors=require('cors');
-const model=require('models');
+const UserModel = require('./models/usuario');
+const db = require("./db/conect")
 
 let app = express();
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
+
 
 //Routes
 app.post('/create', async(req,res)=>{
-
-        let reqs = await model.User.create({
+        let reqs = await UserModel.create({
            'email' : req.body.emailUser,
            'senha' : req.body.emailUser
-        });
-        
+        })
 });
 
 //Start server
 let port = process.env.PORT || 3000;
-app.listen(port,(req,res)=>{
+app.listen(port, async (req,res)=>{
     console.log(port,'Servidor Rodando');
+
+    await db.sync({force: false}) // se quiser que o banco
+
+    console.log("banco conectado");
 })
--- models/usuario.js
+++ models/usuario.js
@@ -1,26 +1,44 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class usuario extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  usuario.init({
-    id: DataTypes.INTEGER,
-    nome: DataTypes.STRING,
-    email: DataTypes.STRING,
-    senha: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'usuario',
-  });
-  return usuario;
-};
+
+// const {
+//   Model
+// } = require('sequelize');
+
+// module.exports = (sequelize, DataTypes) => {
+//   class usuario extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   }
+//   usuario.init({
+//     id: DataTypes.INTEGER,
+//     nome: DataTypes.STRING,
+//     email: DataTypes.STRING,
+//     senha: DataTypes.STRING
+//   }, {
+//     sequelize,
+//     modelName: 'usuario',
+//   });
+//   return usuario;
+// };
+
+
+
+const { Sequelize, DataTypes } = require('sequelize')
+const db = require("../db/conect")
+const User = db.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    unique: true
+  },
+       email: DataTypes.STRING,
+       senha: DataTypes.STRING
+})
+
+module.exports = User
